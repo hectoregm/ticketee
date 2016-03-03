e9cc64a85bdc1fcdcee809cd42b9aeda94abc583
Hector E. Gomez Morales <hectoregm@gmail.com>
2016-03-02 23:46:13 -0600

    Only members of a project can view tickets in it.

--- spec/policies/ticket_policy_spec.rb
+++ spec/policies/ticket_policy_spec.rb
@@ -1,28 +1,48 @@
-require 'spec_helper'
+require 'rails_helper'
 
-describe TicketPolicy do
+RSpec.describe TicketPolicy do
+  context 'permissions' do
+    subject { TicketPolicy.new(user, ticket) }
+    let(:user) { FactoryGirl.create(:user) }
+    let(:project) { FactoryGirl.create(:project) }
+    let(:ticket) { FactoryGirl.create(:ticket, project: project) }
 
-  let(:user) { User.new }
+    context 'for anonymous users' do
+      let(:user) { nil }
 
-  subject { TicketPolicy }
+      it { should_not permit_action :show }
+    end
 
-  permissions ".scope" do
-    pending "add some examples to (or delete) #{__FILE__}"
-  end
+    context 'for viewers of the project' do
+      before { assign_role!(user, :viewer, project) }
 
-  permissions :create? do
-    pending "add some examples to (or delete) #{__FILE__}"
-  end
+      it { should permit_action :show }
+    end
 
-  permissions :show? do
-    pending "add some examples to (or delete) #{__FILE__}"
-  end
+    context 'for editors of the project' do
+      before { assign_role!(user, :editor, project) }
 
-  permissions :update? do
-    pending "add some examples to (or delete) #{__FILE__}"
-  end
+      it { should permit_action :show }
+    end
 
-  permissions :destroy? do
-    pending "add some examples to (or delete) #{__FILE__}"
+    context 'for managers of the project' do
+      before { assign_role!(user, :manager, project) }
+
+      it { should permit_action :show }
+    end
+
+    context 'for managers of other projects' do
+      before do
+        assign_role!(user, :manager, FactoryGirl.create(:project))
+      end
+
+      it { should_not permit_action :show }
+    end
+
+    context 'for administrators' do
+      let(:user) { FactoryGirl.create :user, :admin }
+
+      it { should permit_action :show }
+    end
   end
 end
