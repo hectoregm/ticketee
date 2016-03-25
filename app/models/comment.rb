ad75d8334560f7e3ec27899f3a47eb5cb9a51760
Hector E. Gomez Morales <hectoregm@gmail.com>
2016-03-24 20:18:02 -0600

    Add State model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,7 @@
 class Comment < ActiveRecord::Base
   belongs_to :ticket
   belongs_to :author, class_name: 'User'
+  belongs_to :state
   delegate :project, to: :ticket
 
   validates :text, presence: true
